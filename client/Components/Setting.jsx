e63a81a8a2ba130d009f716c899fa82657be5fbe
Bo Peng <pb0928@gmail.com>
2019-05-08 14:49:46 -0400

    added level description to settings

--- client/Components/Setting.jsx
+++ client/Components/Setting.jsx
@@ -1,16 +1,35 @@
 import React from 'react';
 import graphSetup from '../dthreeHelpers/graphSetup';
 import { withRouter } from 'react-router-dom';
+import { color } from '../dthreeHelpers/graphFunctions';
+
+const styling = {
+  color: 'hsl(0, 0%, 86%)',
+  fontSize: '12px'
+}
 
 const Setting = (props) => {
   const { active, history } = props;
   const display = (active) ? 'is-active' : 'none';
+  const levels = [0,1,2,3,4,5,6,7,8].map((num, i) => {
+    const des = (i === 0) ? ' - Root' : ` - Level ${num} out`;
+    return (
+      <div key={`colorScheme${num}${i}`} className="dropdown-item">
+        <p className="colorScheme"><div style={{background: color(num)}}></div>{des}</p>
+      </div>
+    )
+  });
   return (
     <div className={`dropdown ${display} is-right pointerEventsAuto`}>
       <div className="dropdown-menu" id="dropdown-menu" role="menu">
         <div className="dropdown-content">
           <div className="dropdown-item">
-            <p><strong style={{color:'hsl(0, 0%, 86%)', fontSize:'12px'}}>Options</strong></p>
+            <p><strong style={styling}>Levels</strong></p>
+          </div>
+          {levels}
+          <hr className="dropdown-divider"/>
+          <div className="dropdown-item">
+            <p><strong style={styling}>Options</strong></p>
           </div>
           <a onClick={() => { graphSetup.reset(); props.func(); }} className="dropdown-item">Recenter</a>
           <hr className="dropdown-divider"/>
