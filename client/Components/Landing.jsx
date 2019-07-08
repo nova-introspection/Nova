326ff389e45425732927c2334867f4ee2830b808
Brayton Holman <brayton.holman@gmail.com>
2019-07-08 08:28:03 -0400

    Linting cleanup

--- client/Components/Landing.jsx
+++ client/Components/Landing.jsx
@@ -1,67 +1,72 @@
 import React, { useState } from 'react';
-import Nav from './LandingSections/Nav.jsx';
-import Examples from './LandingSections/Examples.jsx';
-import Description from './LandingSections/Description.jsx';
-// import Contact from './LandingSections/Contact.jsx';
-import Footer from './LandingSections/Footer.jsx';
+import Nav from './LandingSections/Nav';
+import Examples from './LandingSections/Examples';
+import Description from './LandingSections/Description';
+import Footer from './LandingSections/Footer';
 
-const HeroFoot = (props) => (
-  <div className="hero-foot" style={{marginBottom:'25px', paddingLeft: '25px'}}> 
+const HeroFoot = () => (
+  <div className="hero-foot" style={{ marginBottom: '25px', paddingLeft: '25px' }}>
     <div className="container">
-        <p><i className="fas fas fa-arrow-down" style={{color:'turquoise'}}></i> <strong>Demos down below!</strong></p>
+      <p>
+        <i className="fas fas fa-arrow-down" style={{ color: 'turquoise' }} />
+        <strong>Demos down below!</strong>
+      </p>
     </div>
   </div>
-)
+);
 
-const Landing = (props) => {
+const Landing = ({
+  invalidSchema, loadingState, handleUrlClick, history,
+}) => {
   const [urlText, setUrlText] = useState('');
   // conditional rendering for loading/error text
   let loadingText;
-  if (props.invalidSchema) {
+  if (invalidSchema) {
     loadingText = <p className="is-size-5 has-text-danger">Invalid GraphQL endpoint, please try again</p>;
-  } else if (props.loadingState) {
+  } else if (loadingState) {
     loadingText = <p className="is-size-5 has-text-primary">Processing GraphQL Schema...</p>;
-  } else { loadingText = <p className="is-size-5 has-text-dark"><i className="fas fa-star"></i></p>; }
-  const fetching = (props.loadingState && !props.invalidSchema) ? 'is-loading' : '';
+  } else { loadingText = <p className="is-size-5 has-text-dark"><i className="fas fa-star" /></p>; }
+  const fetching = (loadingState && !invalidSchema) ? 'is-loading' : '';
   return (
     <div>
-      <Nav/>
-      <section className="hero is-dark is-fullheight-with-navbar is-mobile"> 
+      <Nav />
+      <section className="hero is-dark is-fullheight-with-navbar is-mobile">
         <div className="hero-body">
           <div className="container">
             <div>
               <h2 id="introspect" className="title">
-                <i style={{color:'turquoise'}} className="fas fa-arrow-alt-circle-down"></i> Introspect an endpoint!
+                <i style={{ color: 'turquoise' }} className="fas fa-arrow-alt-circle-down" />
+                Introspect an endpoint!
               </h2>
             </div>
             <div>
               <div className="field" id="landingInput">
                 <div className={`control has-icons-left ${fetching}`}>
-                  <input 
+                  <input
                     className="input is-primary is-rounded"
                     onChange={(e) => { setUrlText(e.target.value); }}
-                    onKeyDown={(e) => { props.handleUrlClick(urlText, props.history, e); }}
-                    type="text" 
-                    placeholder="Enter Graphql endpoint"/>
-                    <span className="icon is-small is-left">
-                      <i className="fas fa-project-diagram"></i>
-                    </span>
+                    onKeyDown={(e) => { handleUrlClick(urlText, history, e); }}
+                    type="text"
+                    placeholder="Enter Graphql endpoint"
+                  />
+                  <span className="icon is-small is-left">
+                    <i className="fas fa-project-diagram" />
+                  </span>
                 </div>
               </div>
               {loadingText}
             </div>
           </div>
         </div>
-        <HeroFoot/>
+        <HeroFoot />
       </section>
       <div className="container">
-        <Description/>
-        <div className="is-divider"></div>
-        <Examples history={props.history} handleUrlClick={props.handleUrlClick}/>
-        <div className="is-divider"></div>
-        {/* <Contact/> */}
+        <Description />
+        <div className="is-divider" />
+        <Examples history={history} handleUrlClick={handleUrlClick} />
+        <div className="is-divider" />
       </div>
-      <Footer/>
+      <Footer />
     </div>
   );
 };
